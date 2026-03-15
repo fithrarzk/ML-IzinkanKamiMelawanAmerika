d27c8b27b6cb16075d267ef8e61aea37209542f8
Muhammad Fithra Rizki <167663689+fithrarzk@users.noreply.github.com>
2026-03-15 23:58:34 +0700

    fix: memoryview error

--- src/losses.py
+++ src/losses.py
@@ -13,18 +13,22 @@ class Loss:
 class MSE(Loss):
     def forward(self, y_pred: Tensor, y_true: np.ndarray) -> Tensor:
         # Standard MSE aja: mean((ŷ - y)²)
-        y_true_t = Tensor(y_true)
+        y_true_t = Tensor(np.asarray(y_true, dtype=float))
         diff = y_pred - y_true_t
         sq   = diff ** 2
         return sq.mean()
 
 class BCE(Loss):
     def forward(self, y_pred: Tensor, y_true: np.ndarray) -> Tensor:
+        # Pake asarray biar aman dari memoryview (ex. pandas/sklearn split)
+        yp_data = np.asarray(y_pred.data, dtype=float)
+        yt_data = np.asarray(y_true, dtype=float)
+
         # Clip biar gak kena log(0)
-        p      = Tensor(np.clip(y_pred.data, _EPS, 1 - _EPS))
-        y      = Tensor(y_true)
-        one_m_y = Tensor(1.0 - y_true)
-        one_m_p = Tensor(np.clip(1.0 - y_pred.data, _EPS, 1.0))
+        p       = Tensor(np.clip(yp_data, _EPS, 1 - _EPS))
+        y       = Tensor(yt_data)
+        one_m_y = Tensor(1.0 - yt_data)
+        one_m_p = Tensor(np.clip(1.0 - yp_data, _EPS, 1.0))
 
         # Rumus asli BCE: -mean(y*log(p) + (1-y)*log(1-p))
         pos = y * p.log()
@@ -34,13 +38,13 @@ class BCE(Loss):
 
         # Karena ada clipping, autograd butuh bantuan 'hook' sedikit di sini
         _y_pred = y_pred
-        _y_true = y_true
 
         def _bce_backward_hook():
             if _y_pred.requires_grad:
                 _y_pred._ensure_grad()
-                pv = np.clip(_y_pred.data, _EPS, 1 - _EPS)
-                dL = (-((_y_true / pv) - (1 - _y_true) / (1 - pv))) / _y_pred.data.shape[0]
+                yp_arr = np.asarray(_y_pred.data, dtype=float)
+                pv = np.clip(yp_arr, _EPS, 1 - _EPS)
+                dL = (-((yt_data / pv) - (1.0 - yt_data) / (1.0 - pv))) / yp_arr.shape[0]
                 _y_pred.grad += bce_out_grad_ref[0] * dL
 
         # Bikin node output beneran
@@ -57,11 +61,12 @@ class BCE(Loss):
 class CCE(Loss):
     def forward(self, y_pred: Tensor, y_true: np.ndarray) -> Tensor:
         # Categorical Cross-Entropy bwt multi-class
+        yp_data = np.asarray(y_pred.data, dtype=float)
+        yt_data = np.asarray(y_true, dtype=float)
         _y_pred = y_pred
-        _y_true = y_true
 
-        pv  = np.clip(y_pred.data, _EPS, 1.0)
-        val = -float(np.mean(np.sum(y_true * np.log(pv), axis=1)))
+        pv  = np.clip(yp_data, _EPS, 1.0)
+        val = -float(np.mean(np.sum(yt_data * np.log(pv), axis=1)))
 
         out = Tensor(np.array([[val]]), _children=(y_pred,), _op="cce")
 
@@ -69,9 +74,10 @@ class CCE(Loss):
             if _y_pred.requires_grad:
                 _y_pred._ensure_grad()
                 # Gradien normal CCE, tapi DenseLayer biasanya nge-override ini
-                batch = _y_pred.data.shape[0]
-                pv2 = np.clip(_y_pred.data, _EPS, 1.0)
-                _y_pred.grad += out.grad.sum() * (-_y_true / pv2) / batch
+                yp_arr = np.asarray(_y_pred.data, dtype=float)
+                batch = yp_arr.shape[0]
+                pv2 = np.clip(yp_arr, _EPS, 1.0)
+                _y_pred.grad += out.grad.sum() * (-yt_data / pv2) / batch
 
         out._backward = _backward
         return out
